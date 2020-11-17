f2bc5cdce48195d4844ee804d0c6ee07ad6152cb
Rafael Sekiya <rafaelsekiya7070@outlook.com>
2020-11-17 15:53:33 -0300

    App.tsx atualizado

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,8 @@ import Parent from './components/without-redux/Parent';
 import Counter from './components/without-redux/Counter';
 import SimpleHeroesList from './components/without-redux/SimpleHeroesList';
 import TodoList from './components/without-redux/TodoList';
-
+import StyledButton from './components/without-redux/StyledButton';
+import BankAccount from './components/without-redux/BankAccount';
 
 function App() {
   return (
@@ -20,7 +21,9 @@ function App() {
           </Parent> */}
           {/*<SimpleHeroesList />*/}
 
-          <TodoList />
+         {/* <TodoList /> */}
+         {/*< StyledButton backgroundColor={"red"} text={"Clique aqui..."} />*/}
+          < BankAccount initialValue={100} />
       </div>
     </div>
   );
